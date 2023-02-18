e27fd6e7a618e918d401e36bfb3ba08d1a2fc406
alielrayes <engali7assan@yahoo.com>
2023-02-18 20:43:50 +0200

    lesson3

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,72 @@
 import { Box } from "@mui/system";
 import "./Home.css";
 import React from "react";
-import { Paper, Typography, IconButton,   } from "@mui/material";
+import { Paper, Typography, IconButton, Button, Stack } from "@mui/material";
 import { Close } from "@mui/icons-material";
+import { styled, useTheme } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import Avatar from "@mui/material/Avatar";
+
+import { red } from "@mui/material/colors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+
+const receviedDateFromAPI = [{}, {}, {}, {}];
 
 const Home = () => {
- 
+  const theme = useTheme();
+
   return (
-    <Box>
-Home Page
- 
-    </Box>
+    <Stack
+      direction={"row"}
+      sx={{ flexWrap: "wrap", justifyContent: "center" }}
+    >
+      {receviedDateFromAPI.map((item) => {
+        return (
+          <Card sx={{ maxWidth: 277, mb: 6, mx: 2 }}>
+            <CardMedia
+              component="img"
+              height="194"
+              image="/static/images/cards/paella.jpg"
+              alt="Paella dish"
+            />
+            <CardContent>
+              <Typography variant="body2" color="text.secondary">
+                This impressive paella is a perfect party dish and a fun meal to
+                cook together with your guests.
+              </Typography>
+            </CardContent>
+            <CardActions
+              sx={{ justifyContent: "space-between" }}
+              disableSpacing
+            >
+              <Button
+                sx={{ textTransform: "capitalize", p: 1, lineHeight: 1.1 }}
+                variant="contained"
+                color="primary"
+              >
+                Add to cart
+              </Button>
+
+              <Typography
+                mr={1}
+                variant="body1"
+                color={theme.palette.error.light}
+              >
+                $100
+              </Typography>
+            </CardActions>
+          </Card>
+        );
+      })}
+    </Stack>
   );
 };
 
