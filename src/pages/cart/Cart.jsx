a3c146888a4309f5b5a5108d96fbe32ad8e8d917
alielrayes <engali7assan@yahoo.com>
2023-02-19 17:42:15 +0200

    lesson4

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,8 +1,52 @@
-import { Box, Button, InputAdornment, TextField, styled } from "@mui/material";
+import {
+  Box,
+  Button,
+  InputAdornment,
+  Paper,
+  TextField,
+  styled,
+  IconButton,
+  Badge,
+} from "@mui/material";
 import "./Cart.css";
+import { Add, Remove } from "@mui/icons-material";
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {
+    backgroundColor: "#1976d2",
+    color: "#fff",
+  },
+}));
 
 const Cart = () => {
-  return <Box>Cart Page</Box>;
+  return (
+    <Box>
+      <Paper dir="rtl" className="item-container">
+        <div className="img-title-parent">
+          <img src="####" alt="" />
+          <p className="product-name">T-shirt</p>
+        </div>
+
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <IconButton sx={{ color: "#1976d2", ml: "10px" }} onClick={() => {}}>
+            <Add />
+          </IconButton>
+
+          <StyledBadge badgeContent={1} color="secondary" />
+
+          <IconButton sx={{ color: "#1976d2", mr: "10px" }} onClick={() => {}}>
+            <Remove />
+          </IconButton>
+        </div>
+
+        <div className="price">$100</div>
+
+        <Button variant="text" color="error">
+          delete
+        </Button>
+      </Paper>
+    </Box>
+  );
 };
 
 export default Cart;
