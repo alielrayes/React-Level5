2d50c43913616d99d4ee084519b4519678668d94
alielrayes <engali7assan@yahoo.com>
2023-02-22 03:03:43 +0200

    lesson7

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const products = require("./products");
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Welcome our to online shop API...");
+});
+
+app.get("/products", (req, res) => {
+  res.send(products);
+});
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, console.log(`http://localhost:${port}`));
---
+++ backend/products.js
@@ -0,0 +1,35 @@
+const products = [
+  {
+    id: 1,
+    productName: "T-shirt",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+    price: 100,
+    imageLink: "",
+  },
+  {
+    id: 2,
+    productName: "T-shirt",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+    price: 200,
+    imageLink: "",
+  },
+  {
+    id: 3,
+    productName: "T-shirt",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+    price: 300,
+    imageLink: "",
+  },
+  {
+    id: 4,
+    productName: "T-shirt",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+    price: 400,
+    imageLink: "",
+  },
+ ];
+ module.exports = products;
