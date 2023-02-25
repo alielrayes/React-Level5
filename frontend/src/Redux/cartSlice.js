c4184f293a5e9b73078f370e2addb0977b84cbee
alielrayes <engali7assan@yahoo.com>
2023-02-25 18:51:34 +0200

    lesson19

--- frontend/src/Redux/cartSlice.js
+++ frontend/src/Redux/cartSlice.js
@@ -2,45 +2,60 @@ import { createSlice } from "@reduxjs/toolkit";
 
 // use "useSelector" to get the array
 const initialState = {
-  selectedProducts: [
-
-  ],
+  selectedProducts: [],
 };
 
 export const counterSlice = createSlice({
   name: "cart",
   initialState,
-    // action.payload => product From API => القيمة التى بداخل الاقواس
+  // action.payload => product From API => القيمة التى بداخل الاقواس
   reducers: {
     addToCart: (state, action) => {
-  // action.payload => product From API => القيمة التى بداخل الاقواس
-  const productWithQuantity = {...action.payload, "quantity": 1}
-  state.selectedProducts.push(productWithQuantity)
+      // action.payload => product From API => القيمة التى بداخل الاقواس
+      const productWithQuantity = { ...action.payload, quantity: 1 };
+      state.selectedProducts.push(productWithQuantity);
     },
 
     increaseQuantity: (state, action) => {
-      // action.payload => product From user 
+      // action.payload => product From user
       const incresdedProuct = state.selectedProducts.find((item) => {
-        return item.id === action.payload.id
-      })
+        return item.id === action.payload.id;
+      });
 
-      incresdedProuct.quantity +=1
-
+      incresdedProuct.quantity += 1;
     },
 
     decreaseQuantity: (state, action) => {
-  // action.payload => product From user 
-      console.log("doneeeeeeeeeeee")
+      // action.payload => product From user
+      const incresdedProuct = state.selectedProducts.find((item) => {
+        return item.id === action.payload.id;
+      });
+
+      incresdedProuct.quantity -= 1;
+      if (incresdedProuct.quantity === 0) {
+        // delete the selected product
+        const newArr = state.selectedProducts.filter((item) => {
+          return item.id !== action.payload.id;
+        });
+
+        state.selectedProducts = newArr;
+      }
     },
 
     deleteProduct: (state, action) => {
-  // action.payload => product From user 
-      console.log("doneeeeeeeeeeee")
+      // action.payload => product From user
+      // delete the selected product
+      const newArr = state.selectedProducts.filter((item) => {
+        return item.id !== action.payload.id;
+      });
+
+      state.selectedProducts = newArr;
     },
   },
 });
 
 //  دائماً هتنساهااااااااااااااااااااااااااااااااااااااع
-export const {deleteProduct, addToCart, increaseQuantity, decreaseQuantity } = counterSlice.actions;
+export const { deleteProduct, addToCart, increaseQuantity, decreaseQuantity } =
+  counterSlice.actions;
 
 export default counterSlice.reducer;
