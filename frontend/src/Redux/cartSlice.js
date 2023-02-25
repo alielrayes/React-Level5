306c330bde6a64881cc771cf076f8c3fcd7c5226
alielrayes <engali7assan@yahoo.com>
2023-02-25 15:07:45 +0200

    lesson17

--- frontend/src/Redux/cartSlice.js
+++ frontend/src/Redux/cartSlice.js
@@ -3,7 +3,22 @@ import { createSlice } from "@reduxjs/toolkit";
 // use "useSelector" to get the array
 const initialState = {
   selectedProducts: [
-  
+    {
+      "id": 1,
+      "productName": "T-shirt",
+      "description": "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+      "price": 100,
+      "imageLink": "https://res.cloudinary.com/dpm5z8e71/image/upload/v1677029702/Online%20store/4_y2jc81.jpg",
+      "quantity": 1
+      },
+      {
+      "id": 2,
+      "productName": "T-shirt",
+      "description": "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+      "price": 200,
+      "imageLink": "https://res.cloudinary.com/dpm5z8e71/image/upload/v1677029701/Online%20store/3_gvu4ga.jpg",
+      "quantity": 1
+      },
   ],
 };
 
@@ -14,18 +29,18 @@ export const counterSlice = createSlice({
   reducers: {
     addToCart: (state, action) => {
   // action.payload => product From API => القيمة التى بداخل الاقواس
-
   const productWithQuantity = {...action.payload, "quantity": 1}
   state.selectedProducts.push(productWithQuantity)
-  
-  
-  
-  console.log("doneeeeeeeeeeee")
     },
 
     increaseQuantity: (state, action) => {
       // action.payload => product From user 
-      console.log("doneeeeeeeeeeee")
+      const incresdedProuct = state.selectedProducts.find((item) => {
+        return sssssssssssssssssss
+      })
+
+      incresdedProuct.quantity +=1
+
     },
 
     decreaseQuantity: (state, action) => {
