062c9e4f43daa92ad0930ae008e750d9c2850b51
alielrayes <engali7assan@yahoo.com>
2023-02-26 02:48:31 +0200

    lesson20

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,85 +1,128 @@
 import { Box } from "@mui/system";
 import "./Home.css";
 import React from "react";
-import { Paper, Typography, IconButton, Button, Stack, CircularProgress } from "@mui/material";
-import { Close } from "@mui/icons-material";
+import {
+  Paper,
+  Typography,
+  IconButton,
+  Button,
+  Stack,
+  CircularProgress,
+  Badge,
+} from "@mui/material";
+import { Add, Close, Remove } from "@mui/icons-material";
 import { styled, useTheme } from "@mui/material/styles";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
-import { useGetproductsByNameQuery } from '../../Redux/productsApi'
-import { useDispatch } from "react-redux";
-import { addToCart } from "Redux/cartSlice";
+import { useGetproductsByNameQuery } from "../../Redux/productsApi";
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart, decreaseQuantity, increaseQuantity } from "Redux/cartSlice";
 
- 
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {},
+}));
 
 const Home = () => {
   const theme = useTheme();
-  const { data, error, isLoading } = useGetproductsByNameQuery()
-  const dispatch = useDispatch()
+  const { data, error, isLoading } = useGetproductsByNameQuery();
+  const dispatch = useDispatch();
+  const { selectedProducts } = useSelector((state) => state.carttt);
+
+
   
+  if (isLoading) {
+    return (
+      <Box sx={{ display: "flex" }}>
+        <CircularProgress />
+      </Box>
+    );
+  }
 
-if (isLoading) {
-  return(
-    <Box sx={{ display: 'flex' }}>
-    <CircularProgress />
-  </Box>
-  )
-}
-
-
-
-if (data) {
-  return (
-    <Stack
-      direction={"row"}
-      sx={{ flexWrap: "wrap", justifyContent: "center" }}
-    >
-      {data.map((item) => {
-        return (
-          <Card className="card" key={item.id} sx={{ maxWidth: 277, mb: 6, mx: 2 }}>
-            <CardMedia
-              component="img"
-              height="277"
-              image={item.imageLink}
-              alt="Paella dish"
-            />
-            <CardContent>
-              <Typography variant="body2" color="text.secondary">
-                {item.description}
-              </Typography>
-            </CardContent>
-            <CardActions
-              sx={{ justifyContent: "space-between" }}
-              disableSpacing
+  if (data) {
+    return (
+      <Stack
+        direction={"row"}
+        sx={{ flexWrap: "wrap", justifyContent: "center" }}
+      >
+        {data.map((item) => {
+          return (
+            <Card
+              className="card"
+              key={item.id}
+              sx={{ maxWidth: 277, mb: 6, mx: 2 }}
             >
-              <Button
-                sx={{ textTransform: "capitalize", p: 1, lineHeight: 1.1 }}
-                variant="contained"
-                color="primary"
-                onClick={() => {
-                  dispatch(addToCart(item))
-                }}
+              <CardMedia
+                component="img"
+                height="277"
+                image={item.imageLink}
+                alt="Paella dish"
+              />
+              <CardContent>
+                <Typography variant="body2" color="text.secondary">
+                  {item.description}
+                </Typography>
+              </CardContent>
+              <CardActions
+                sx={{ justifyContent: "space-between" }}
+                disableSpacing
               >
-                Add to cart
-              </Button>
+                {selectedProducts.includes(item) ? (
+                  <div
+                    dir="rtl"
+                    style={{ display: "flex", alignItems: "center" }}
+                  >
+                    <IconButton
+                      color="primary"
+                      sx={{ ml: "10px" }}
+                      onClick={() => {
+                        dispatch(increaseQuantity(item));
+                      }}
+                    >
+                      <Add fontSize="small" />
+                    </IconButton>
 
-              <Typography
-                mr={1}
-                variant="body1"
-                color={theme.palette.error.light}
-              >
-                ${item.price}
-              </Typography>
-            </CardActions>
-          </Card>
-        );
-      })}
-    </Stack>
-  );
-}
+                    <StyledBadge badgeContent={1} color="primary" />
+
+                    <IconButton
+                      color="primary"
+                      sx={{ mr: "10px" }}
+                      onClick={() => {
+                        dispatch(decreaseQuantity(item));
+                      }}
+                    >
+                      <Remove fontSize="small" />
+                    </IconButton>
+                  </div>
+                ) : (
+                  <Button
+                    sx={{ textTransform: "capitalize", p: 1, lineHeight: 1.1 }}
+                    variant="contained"
+                    color="primary"
+                    onClick={() => {
+                      dispatch(addToCart(item));
+                    }}
+                  >
+                    Add to cart
+                  </Button>
+                )}
+
+                <Typography
+                  mr={1}
+                  variant="body1"
+                  color={theme.palette.error.light}
+                >
+                  ${item.price}
+                </Typography>
+              </CardActions>
+            </Card>
+          );
+        })}
+      </Stack>
+    );
+  }
 };
 
 export default Home;
