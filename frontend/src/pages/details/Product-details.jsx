2a6cfb0f46f4f1ffc1d0177bb45dd7fc27c19e65
alielrayes <engali7assan@yahoo.com>
2023-03-04 15:57:40 +0200

    lesson34

--- frontend/src/pages/details/Product-details.jsx
+++ frontend/src/pages/details/Product-details.jsx
@@ -2,9 +2,32 @@ import React, { useRef, useState } from "react";
 import "./product-details.css";
 import { useGetOneProductQuery } from "Redux/productsApi";
 import { useParams } from "react-router-dom";
-import { Box, CircularProgress, Typography } from "@mui/material";
+import {
+  Badge,
+  Box,
+  Button,
+  CircularProgress,
+  IconButton,
+  Typography,
+  styled,
+} from "@mui/material";
 import DetailsThumb from "./DetailsThumb";
+
+import { useDispatch, useSelector } from "react-redux";
+
+import { addToCart, decreaseQuantity, increaseQuantity } from "Redux/cartSlice";
+import { Add, Remove, ShoppingCart } from "@mui/icons-material";
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {},
+}));
+
 const ProductDetails = () => {
+  const dispatch = useDispatch();
+  const { selectedProducts, selectedProductsID } = useSelector(
+    // @ts-ignore
+    (state) => state.carttt
+  );
   let { id } = useParams();
   // data => only one product
   const { data, error, isLoading } = useGetOneProductQuery(id);
@@ -20,6 +43,14 @@ const ProductDetails = () => {
       images[i].className = images[i].className.replace("active", "");
     }
     images[index].className = "active";
+  };
+
+  const productQuantity = (itemAPI) => {
+    const myProduct = selectedProducts.find((itemUser) => {
+      return itemUser.id === itemAPI.id;
+    });
+
+    return myProduct.quantity;
   };
 
   if (isLoading) {
@@ -53,7 +84,7 @@ const ProductDetails = () => {
               <h2>{data.productName}</h2>
               <span>${data.price}</span>
             </div>
-            {/* <Colors colors={item.colors} /> */}
+            {/* <Colors colors={data.colors} /> */}
 
             <p>{data.description}</p>
 
@@ -62,7 +93,52 @@ const ProductDetails = () => {
               tab={handleTab}
               myRef={myRef}
             />
-            <button className="cart">Add to cart</button>
+            {/* <button className="cart">Add to cart</button> */}
+            {selectedProductsID.includes(data.id) ? (
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  marginTop: "33px",
+                }}
+              >
+                <IconButton
+                  color="primary"
+                  sx={{ mr: "10px" }}
+                  onClick={() => {
+                    dispatch(decreaseQuantity(data));
+                  }}
+                >
+                  <Remove fontSize="small" />
+                </IconButton>
+
+                <StyledBadge
+                  badgeContent={productQuantity(data)}
+                  color="primary"
+                />
+
+                <IconButton
+                  color="primary"
+                  sx={{ ml: "10px" }}
+                  onClick={() => {
+                    dispatch(increaseQuantity(data));
+                  }}
+                >
+                  <Add fontSize="small" />
+                </IconButton>
+              </div>
+            ) : (
+              <Button
+                sx={{ textTransform: "capitalize", p: 1, lineHeight: 1.1 }}
+                variant="contained"
+                color="primary"
+                onClick={() => {
+                  dispatch(addToCart(data));
+                }}
+              >
+                <ShoppingCart sx={{ fontSize: "18px", mr: 1 }} /> Add to cart
+              </Button>
+            )}
           </div>
         </div>
       </div>
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -9,7 +9,7 @@ import {
   CircularProgress,
   Badge,
 } from "@mui/material";
-import { Add, Remove } from "@mui/icons-material";
+import { Add, Remove, ShoppingCart } from "@mui/icons-material";
 import { styled, useTheme } from "@mui/material/styles";
 import Card from "@mui/material/Card";
 
@@ -134,7 +134,8 @@ const Home = () => {
                       dispatch(addToCart(item));
                     }}
                   >
-                    Add to cart
+                    <ShoppingCart sx={{ fontSize: "18px", mr: 1 }} /> Add to
+                    cart
                   </Button>
                 )}
 
